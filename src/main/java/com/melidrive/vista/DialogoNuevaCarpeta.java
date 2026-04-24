59aaeb7d719f0a7d7e81e36581379178106e37fe
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:56:53 -0400

    SubClase Mostrar

--- src/main/java/com/melidrive/vista/DialogoNuevaCarpeta.java
+++ src/main/java/com/melidrive/vista/DialogoNuevaCarpeta.java
@@ -7,4 +7,21 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class DialogoNuevaCarpeta {
+    public void mostrar() {
+
+        Stage ventana = new Stage();
+        ventana.initModality(Modality.APPLICATION_MODAL);
+
+        Label label = new Label("Nombre de la carpeta:");
+        TextField campo = new TextField();
+
+        Button btnCrear = new Button("Crear");
+        Button btnCancelar = new Button("Cancelar");
+
+        btnCancelar.setOnAction(e -> ventana.close());
+        VBox root = new VBox(10, label, campo, btnCrear, btnCancelar);
+
+        ventana.setScene(new Scene(root, 300, 200));
+        ventana.showAndWait();
+    }
 }
