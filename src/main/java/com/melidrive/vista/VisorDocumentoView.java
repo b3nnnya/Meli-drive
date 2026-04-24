5f3bc5e22bb3cea7cb07d6bb3ac78f527b549a6a
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 13:03:12 -0400

    Creacion VisorDocumentoView

--- src/main/java/com/melidrive/vista/VisorDocumentoView.java
+++ src/main/java/com/melidrive/vista/VisorDocumentoView.java
@@ -5,4 +5,14 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class VisorDocumentoView extends HBox {
+
+    public VisorDocumentoView() {
+
+        TextArea areaTexto = new TextArea();
+        areaTexto.setEditable(false);
+
+        VBox panelEtiquetas = new VBox();
+
+        getChildren().addAll(areaTexto, panelEtiquetas);
+    }
 }
