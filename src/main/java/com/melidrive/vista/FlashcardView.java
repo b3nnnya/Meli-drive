72a3aa848aac849e7c2b233239098f4a1d93ff63
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:48:40 -0400

    SubClase FlashcardView

--- src/main/java/com/melidrive/vista/FlashcardView.java
+++ src/main/java/com/melidrive/vista/FlashcardView.java
@@ -7,6 +7,25 @@ import javafx.scene.layout.VBox;
 
 public class FlashcardView extends VBox {
     private boolean mostrandoPregunta = true;
-    public FlashcardView () {}
+    public FlashcardView () {
+        StackPane tarjeta = new StackPane();
+        Label contenido = new Label("Pregunta");
+
+        tarjeta.getChildren().add(contenido);
+
+        tarjeta.setOnMouseClicked(e -> {
+            if (mostrandoPregunta) {
+                contenido.setText("Respuesta");
+            } else {
+                contenido.setText("Pregunta");
+            }
+            mostrandoPregunta = !mostrandoPregunta;
+        });
+
+        Button btnBien = new Button("Fácil");
+        Button btnMal = new Button("Difícil");
+
+        getChildren().addAll(tarjeta, btnBien, btnMal);
+    }
 
 }
