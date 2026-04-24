f9f87b88027b524dd091e58bea19f543e3e70b64
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:43:04 -0400

    Creacion de FlashcardView

---
+++ src/main/java/com/melidrive/vista/FlashcardView.java
@@ -0,0 +1,9 @@
+package com.melidrive.vista;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class FlashcardView extends VBox {
+}
