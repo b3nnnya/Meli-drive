43e9b3f89712bf9f2a223e61a62cc482a014ba9c
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:09:27 -0400

    StackPane

--- src/main/java/com/melidrive/vista/MainView.java
+++ src/main/java/com/melidrive/vista/MainView.java
@@ -1,8 +1,13 @@
 package com.melidrive.vista;
 
+import com.sun.tools.javac.Main;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
 public class MainView {
+    private StackPane areaCentral;
+    public MainView() {
+
+    }
 }
