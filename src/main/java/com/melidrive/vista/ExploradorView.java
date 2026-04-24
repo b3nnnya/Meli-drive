7b53a9ae434f99c9827f2cd81809cff839ae8c56
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:38:40 -0400

    ExploardorView

--- src/main/java/com/melidrive/vista/ExploradorView.java
+++ src/main/java/com/melidrive/vista/ExploradorView.java
@@ -6,6 +6,14 @@ import javafx.scene.layout.VBox;
 
 public class ExploradorView extends VBox {
     private TableView<String> tabla;
-    public ExploradorView() {}
+    public ExploradorView() {
+        tabla = new TableView<>();
+
+        TableColumn<String, String> colNombre = new TableColumn<>("Nombre");
+
+        tabla.getColumns().add(colNombre);
+
+        getChildren().add(tabla);
+    }
     public void cargarArchivos() {}
 }
