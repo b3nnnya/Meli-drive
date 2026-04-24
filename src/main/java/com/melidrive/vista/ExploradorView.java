32d7245d14d5cfb91936e3daf752aa87ad0759dc
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:36:02 -0400

    Creacion de Subclases

--- src/main/java/com/melidrive/vista/ExploradorView.java
+++ src/main/java/com/melidrive/vista/ExploradorView.java
@@ -5,5 +5,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 
 public class ExploradorView extends VBox {
-
+    private TableView<String> tabla;
+    public ExploradorView() {}
+    public void cargarArchivos() {}
 }
