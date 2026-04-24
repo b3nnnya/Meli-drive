f244724ada6adca3b9b526d9daff391e2b2a29d1
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:32:32 -0400

    Creacion de ExploradorView

---
+++ src/main/java/com/melidrive/vista/ExploradorView.java
@@ -0,0 +1,9 @@
+package com.melidrive.vista;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+public class ExploradorView extends VBox {
+
+}
