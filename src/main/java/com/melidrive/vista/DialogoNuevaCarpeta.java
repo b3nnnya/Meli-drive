6434ddfcf0925b3550c5492f95b666cb554b9972
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-24 12:51:35 -0400

    Creacion DialogoNuevaCarpeta

---
+++ src/main/java/com/melidrive/vista/DialogoNuevaCarpeta.java
@@ -0,0 +1,10 @@
+package com.melidrive.vista;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class DialogoNuevaCarpeta {
+}
