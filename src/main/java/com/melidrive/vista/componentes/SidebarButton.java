6d97b1f9ac0f4da6126353c6409304d57ea6b4a0
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-05-29 11:08:05 -0400

    SidebarButton

--- src/main/java/com/melidrive/vista/componentes/SidebarButton.java
+++ src/main/java/com/melidrive/vista/componentes/SidebarButton.java
@@ -2,4 +2,21 @@ package com.melidrive.vista.componentes;
 
 import javafx.scene.control.Button;
 
-public class SidebarButton extends Button {}
+public class SidebarButton extends Button {
+
+    public SidebarButton(String texto) {
+        super(texto);
+        getStyleClass().add("sidebar-button");
+        setMaxWidth(Double.MAX_VALUE);
+    }
+
+    public void activar() {
+        if (!getStyleClass().contains("sidebar-button-active")) {
+            getStyleClass().add("sidebar-button-active");
+        }
+    }
+
+    public void desactivar() {
+        getStyleClass().remove("sidebar-button-active");
+    }
+}
