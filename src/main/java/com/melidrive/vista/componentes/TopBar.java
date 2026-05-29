dfbb7c747562fa3ce4f93ba3238902855bcba34f
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-05-29 11:09:46 -0400

    TopBar

--- src/main/java/com/melidrive/vista/componentes/TopBar.java
+++ src/main/java/com/melidrive/vista/componentes/TopBar.java
@@ -1,4 +1,38 @@
 package com.melidrive.vista.componentes;
 
-public class TopBar {
+import com.melidrive.controlador.MainController;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+
+public class TopBar extends HBox {
+
+    public TopBar(MainController mainController) {
+
+        getStyleClass().add("topbar");
+        setAlignment(Pos.CENTER_LEFT);
+        setSpacing(12);
+
+        TextField buscador = new TextField();
+        buscador.setPromptText("Buscar archivos...");
+        buscador.getStyleClass().add("search-field");
+
+        Button buscar = new Button("Buscar");
+        buscar.getStyleClass().add("primary-button");
+
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, javafx.scene.layout.Priority.ALWAYS);
+
+        Button darkMode = new Button("🌙");
+        darkMode.getStyleClass().add("secondary-button");
+
+        getChildren().addAll(
+                buscador,
+                buscar,
+                spacer,
+                darkMode
+        );
+    }
 }
