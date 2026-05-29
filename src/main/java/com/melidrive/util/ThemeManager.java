38d457aa225c0b7c983b9ee5ca7539343c3e409c
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-05-29 11:22:29 -0400

    ThemeManager

--- src/main/java/com/melidrive/util/ThemeManager.java
+++ src/main/java/com/melidrive/util/ThemeManager.java
@@ -1,3 +1,23 @@
 package com.melidrive.util;
 
-public class ThemeManager { }
+import javafx.scene.Scene;
+
+public class ThemeManager {
+
+    public static void aplicarDarkMode(Scene scene) {
+
+        scene.getStylesheets().clear();
+
+        scene.getStylesheets().add(
+                ThemeManager.class
+                        .getResource("/styles/main.css")
+                        .toExternalForm()
+        );
+
+        scene.getStylesheets().add(
+                ThemeManager.class
+                        .getResource("/styles/dark.css")
+                        .toExternalForm()
+        );
+    }
+}
