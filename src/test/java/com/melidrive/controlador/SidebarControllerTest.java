44fb27c8a34dbd759480f040b0522f12b4bf844c
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-06-18 10:11:49 -0400

    SidebarControllerTest

--- src/test/java/com/melidrive/controlador/SidebarControllerTest.java
+++ src/test/java/com/melidrive/controlador/SidebarControllerTest.java
@@ -1,4 +1,64 @@
 package com.melidrive.controlador;
 
-public class SidebarControllerTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SidebarControllerTest {
+
+    @Test
+    void debeIniciarEnMiUnidad() {
+
+        SidebarController controller =
+                new SidebarController(new MainController());
+
+        assertEquals(
+                SidebarController.SeccionApp.MI_UNIDAD,
+                controller.getSeccionActiva()
+        );
+    }
+
+    @Test
+    void irAModoEstudioDebeActualizarSeccion() {
+
+        SidebarController controller =
+                new SidebarController(new MainController());
+
+        controller.irAModoEstudio();
+
+        assertEquals(
+                SidebarController.SeccionApp.MODO_ESTUDIO,
+                controller.getSeccionActiva()
+        );
+    }
+
+    @Test
+    void irABuscarPorEtiquetaDebeActualizarSeccion() {
+
+        SidebarController controller =
+                new SidebarController(new MainController());
+
+        controller.irABuscarPorEtiqueta();
+
+        assertEquals(
+                SidebarController.SeccionApp.BUSCAR_POR_ETIQUETA,
+                controller.getSeccionActiva()
+        );
+    }
+
+    @Test
+    void navegarADebeCambiarCorrectamente() {
+
+        SidebarController controller =
+                new SidebarController(new MainController());
+
+        controller.navegarA(
+                SidebarController.SeccionApp.MODO_ESTUDIO
+        );
+
+        assertEquals(
+                SidebarController.SeccionApp.MODO_ESTUDIO,
+                controller.getSeccionActiva()
+        );
+    }
 }
